e9b3b599a3f7d4f89afb938c9ff50d7b45a5a202
Ivy Patton <lilaivy@gmail.com>
2017-08-24 13:09:28 -0700

    corrected render bracketts

--- purepoint-recipe-finder/src/App.js
+++ purepoint-recipe-finder/src/App.js
@@ -15,34 +15,34 @@ class App extends Component {
   }
 
   updatebox(value) {
-    this.setState({searchQuery: value})
-    let {searchQuery} = this.state;
+    this.setState({ searchQuery: value })
+    let { searchQuery } = this.state;
     api.getLists(value)
-    .then(res => {
-      this.setState({searchList: res});
-    })
-    .catch(err => console.log(err));
-    }
+      .then(res => {
+        this.setState({ searchList: res });
+      })
+      .catch(err => console.log(err));
   }
+
 
   render() {
     return (
       <div className="App">
-        <SearchBar updatebox={this.updatebox} value={this.state.searchQuery}/>
+        <SearchBar updatebox={this.updatebox} value={this.state.searchQuery} />
         <div className="SearchContainer">
-        {this.state.searchList.map((recipeItem, i) => {
-          return(
-            <div key={i} className="RecipeItemContainer" data-recipe={recipeItem.id} onCLick={ e => {
-              
-              e.preventDefault();
-              this.selectRecipe(e.target.getAttribute('data-recipe'));
+          {this.state.searchList.map((recipeItem, i) => {
+            return (
+              <div key={i} className="RecipeItemContainer" data-recipe={recipeItem.id} onCLick={e => {
+
+                e.preventDefault();
+                this.selectRecipe(e.target.getAttribute('data-recipe'));
               }}>
-               <img src={`${recipeItem.thumbnail}`} className="RecipePic" data-recipe={recipeItem.id}/>
+                <img src={`${recipeItem.thumbnail}`} className="RecipePic" data-recipe={recipeItem.id} />
                 <h4 className="RecipeTitle" data-recipe={recipeItem.id}>{recipeItem.title}</h4>
                 <h4 className="Ingredients" data-recipe={recipeItem.id}>{recipeItem.ingredients}</h4>
               </div>
-          )
-        })}
+            );
+          })}
           <h2>Welcome to Purepoint Recipe Finder</h2>
         </div>
         <p className="App-intro">
@@ -51,6 +51,7 @@ class App extends Component {
       </div>
     );
   }
+}
 
 
 export default App;
